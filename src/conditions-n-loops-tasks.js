32520c2837f18ff9ea745e3f0588cccc5cc4f189
dyoncha <dyoncha77@gmail.com>
2024-12-08 14:52:20 +0300

    feat: add isContainNumber fn

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -273,8 +273,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const stringNum = `${num}`;
+  for (let i = 0; i < stringNum.length; i += 1) {
+    if (+stringNum[i] === digit) {
+      return true;
+    }
+  }
+  return false;
 }
 
 /**
